181f7a8ccd28273a731d1bf1cd28be476fbc4cd1
Jake Hansen <JakeMHansen95@gmail.com>
2024-02-06 21:19:53 -0600

    Setup discord bot

---
+++ src/discord.rs
@@ -0,0 +1,125 @@
+use crate::docker;
+use anyhow::Result;
+use poise::serenity_prelude::{self as serenity, Mention};
+use std::time::Duration;
+use tracing::info;
+
+struct Data {}
+type Error = Box<dyn std::error::Error + Send + Sync>;
+type Context<'a> = poise::Context<'a, Data, Error>;
+pub async fn create_client(discord_token: &str) -> Result<serenity::Client> {
+    let intents = serenity::GatewayIntents::non_privileged();
+    let framework = poise::Framework::builder()
+        .options(poise::FrameworkOptions {
+            commands: vec![
+                status(),
+                start(),
+                stop(),
+                //    save()
+            ],
+            ..Default::default()
+        })
+        .setup(|ctx, _ready, framework| {
+            Box::pin(async move {
+                poise::builtins::register_globally(ctx, &framework.options().commands).await?;
+                Ok(Data {})
+            })
+        })
+        .build();
+    let client = serenity::ClientBuilder::new(discord_token, intents)
+        .framework(framework)
+        .await?;
+    Ok(client)
+}
+
+/// Gets the status of the palworld server
+#[poise::command(prefix_command, slash_command)]
+async fn status(ctx: Context<'_>) -> Result<(), Error> {
+    info!("status command called by {}", ctx.author().name);
+    ctx.defer().await?;
+    let server_running = docker::get_palworld_docker_container(None).await.is_ok();
+    if server_running {
+        if let Ok((players, count)) = docker::get_players().await {
+            let memory = docker::get_memory_stats()?;
+            ctx.say(format!(
+                "```\n{count} players online\n{players}\n---\n{memory}\n```"
+            ))
+            .await?;
+        } else {
+            ctx.say("Server is booting/restarting").await?;
+        }
+    } else {
+        ctx.say("Server Offline").await?;
+    }
+    Ok(())
+}
+/// Starts server if it is offline
+#[poise::command(prefix_command, slash_command)]
+async fn start(ctx: Context<'_>) -> Result<(), Error> {
+    info!("start command called");
+    ctx.defer().await?;
+    if docker::get_palworld_docker_container(None).await.is_ok() {
+        ctx.say("Server is already running").await?;
+    } else {
+        let res = docker::start_docker_container().await;
+        match res {
+            Ok(_) => {
+                info!("Server starting");
+                ctx.say("Server is starting").await?;
+                while docker::run_rcon_command(vec!["info"]).await.is_err() {
+                    tokio::time::sleep(Duration::from_secs(1)).await;
+                }
+                info!("Server Started");
+                ctx.reply(format!("Server started {}", Mention::from(ctx.author().id)))
+                    .await?
+            }
+            Err(e) => ctx.say(format!("Server failed to start {:?}", e)).await?,
+        };
+    };
+    Ok(())
+}
+
+/// Saves and stops the server if all players are offline
+#[poise::command(prefix_command, slash_command)]
+async fn stop(ctx: Context<'_>) -> Result<(), Error> {
+    info!("stop command called");
+    ctx.defer().await?;
+    if let Ok((players, count)) = docker::get_players().await {
+        if count != 0 {
+            ctx.say(format!("{count} players are still online\n{players}"))
+                .await?;
+        } else {
+            ctx.say("Server shutting down").await?;
+            docker::stop_docker_container().await?;
+            while docker::run_rcon_command(vec!["info"]).await.is_ok() {
+                tokio::time::sleep(Duration::from_secs(1)).await;
+            }
+            info!("Server Stopped");
+            ctx.reply(format!("Server Stopped {}", Mention::from(ctx.author().id)))
+                .await?;
+        }
+    } else {
+        ctx.say("Server is already offline").await?;
+    }
+    Ok(())
+}
+/*
+
+/// Saves the game
+#[poise::command(prefix_command, slash_command)]
+async fn save(ctx: Context<'_>) -> Result<(), Error> {
+    info!("save command called");
+    ctx.defer().await?;
+    let mut ses = create_ssh_session(&ctx.data().ip, &ctx.data().password)?;
+
+    let is_fully_running = is_docker_running(&mut ses)? && is_server_responding(&mut ses)?;
+
+    if is_fully_running {
+        let result = save_server(&mut ses)?;
+        ctx.say(result).await?;
+    } else {
+        ctx.say("Server is offline").await?;
+    }
+    Ok(())
+}
+*/
---
+++ src/docker.rs
@@ -0,0 +1,131 @@
+use anyhow::{bail, Context as _, Result};
+use bollard::{
+    container::ListContainersOptions,
+    exec::{CreateExecOptions, StartExecResults},
+    service::ContainerSummary,
+    Docker,
+};
+use futures_util::stream::StreamExt;
+use itertools::Itertools;
+use serde::ser::Serialize;
+use std::{process::Command, str};
+use tracing::info;
+
+pub async fn get_palworld_docker_container(docker: Option<&Docker>) -> Result<ContainerSummary> {
+    let opt;
+    let docker = match docker {
+        Some(d) => d,
+        None => {
+            opt = Docker::connect_with_local_defaults()?;
+            &opt
+        }
+    };
+    let container = docker
+        .list_containers(Some(ListContainersOptions::<String> {
+            all: true,
+            ..Default::default()
+        }))
+        .await?
+        .into_iter()
+        .filter(|c| match (&c.image, &c.names) {
+            (Some(image_name), Some(names)) => {
+                image_name == "thijsvanloef/palworld-server-docker:latest"
+                    && names.iter().any(|name| name == "/palworld-server")
+            }
+            _ => false,
+        })
+        .at_most_one()?
+        .context("No container found")?;
+    Ok(container)
+}
+
+pub async fn start_docker_container() -> Result<()> {
+    let docker = Docker::connect_with_local_defaults()?;
+    docker
+        .start_container::<String>("palworld-server", None)
+        .await
+        .context("Failed to start container")?;
+    Ok(())
+}
+
+pub async fn stop_docker_container() -> Result<()> {
+    let docker = Docker::connect_with_local_defaults()?;
+    docker
+        .stop_container("palworld-server", None)
+        .await
+        .context("Failed to stop container")?;
+    Ok(())
+}
+
+pub async fn get_players() -> Result<(String, usize)> {
+    let data = run_rcon_command(vec!["ShowPlayers"]).await?;
+    let response = data.lines().collect::<Vec<&str>>();
+    // First line is csv header, don't count it in character count
+    let count = response.len() - 1;
+    let players = response.join("\n");
+    Ok((players, count))
+}
+
+pub fn get_memory_stats() -> Result<String> {
+    let output = Command::new("free").arg("-h").output()?.stdout;
+    Ok(String::from_utf8(output)?)
+}
+
+pub async fn run_rcon_command(mut command: Vec<&str>) -> Result<String> {
+    let mut rcon_command = vec!["rcon-cli"];
+    rcon_command.append(&mut command);
+    let res = exec_command(rcon_command).await?;
+    if res.is_empty() {
+        bail!("No rcon response");
+    } else {
+        Ok(res)
+    }
+}
+async fn exec_command<T>(command: Vec<T>) -> Result<String>
+where
+    T: Into<String> + Serialize + Default,
+{
+    let docker = Docker::connect_with_local_defaults()?;
+    let container = get_palworld_docker_container(Some(&docker)).await?;
+    info!("{:?}", container.state);
+    if container.is_running() {
+        let id = container.id.unwrap();
+        let exec = docker
+            .create_exec(
+                &id,
+                CreateExecOptions {
+                    attach_stdout: Some(true),
+                    cmd: Some(command),
+                    ..Default::default()
+                },
+            )
+            .await?
+            .id;
+        let mut out = String::new();
+        if let StartExecResults::Attached { mut output, .. } =
+            docker.start_exec(&exec, None).await?
+        {
+            while let Some(Ok(msg)) = output.next().await {
+                out.push_str(str::from_utf8(msg.as_ref())?);
+            }
+        } else {
+            unreachable!();
+        }
+        Ok(out)
+    } else {
+        bail!("Container not running")
+    }
+}
+
+trait ContainerIsRunning {
+    fn is_running(&self) -> bool;
+}
+
+impl ContainerIsRunning for ContainerSummary {
+    fn is_running(&self) -> bool {
+        match &self.state {
+            Some(state) => state.as_str() == "running",
+            None => false,
+        }
+    }
+}
